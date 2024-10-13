86032ae489805ff302e2ba1e264867a1db7a0053
KIAwhite58 <120634588+KIAwhite58@users.noreply.github.com>
2024-10-13 14:56:27 +0800

    Update hashmaps3.rs

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -34,6 +34,29 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+
+       // 更新 team_1 的得分和失分
+        if let Some(team_1) = scores.get_mut(&team_1_name) {
+            team_1.goals_scored += team_1_score;
+            team_1.goals_conceded += team_2_score;
+        } else {
+            scores.insert(team_1_name, Team {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+        }
+
+        // 更新 team_2 的得分和失分
+        if let Some(team_2) = scores.get_mut(&team_2_name) {
+            team_2.goals_scored += team_2_score;
+            team_2.goals_conceded += team_1_score;
+        } else {
+            scores.insert(team_2_name, Team {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
+        }
+    
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
