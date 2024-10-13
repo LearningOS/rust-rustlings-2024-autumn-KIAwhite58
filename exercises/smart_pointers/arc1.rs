637e0613332effc4553d67f91661cecf0653f5dc
KIAwhite58 <120634588+KIAwhite58@users.noreply.github.com>
2024-10-13 22:34:08 +0800

    Update arc1.rs

--- exercises/smart_pointers/arc1.rs
+++ exercises/smart_pointers/arc1.rs
@@ -28,14 +28,14 @@ use std::sync::Arc;
 use std::thread;
 
 fn main() {
-    let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let numbers: Vec<u32> = (0..100u32).collect();
+    let shared_numbers = Arc::new(numbers); // 使用 Arc 包裹 numbers 向量
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
-        let child_numbers = // TODO
+        let child_numbers = Arc::clone(&shared_numbers); // 克隆 Arc 引用，不需要复制数据
         joinhandles.push(thread::spawn(move || {
-            let sum: u32 = child_numbers.iter().filter(|&&n| n % 8 == offset).sum();
+            let sum: u32 = child_numbers.iter().filter(|&&n| (n + offset) % 8 == offset).sum();
             println!("Sum of offset {} is {}", offset, sum);
         }));
     }
