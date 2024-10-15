d4cfe936c2816ceea1302faaf5de8fded9b396f8
KIAwhite58 <120634588+KIAwhite58@users.noreply.github.com>
2024-10-15 16:27:33 +0800

    Update try_from_into.rs

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -41,23 +41,57 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (red, green, blue) = tuple;
+        if red < 0 || green < 0 || blue < 0 || red > 255 || green > 255 || blue > 255 {
+            Err(IntoColorError::IntConversion)
+        } else {
+            Ok(Color {
+                red: red as u8,
+                green: green as u8,
+                blue: blue as u8,
+            })
+        }
     }
 }
 
-// Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let red = arr[0];
+        let green = arr[1];
+        let blue = arr[2];
+        if red < 0 || green < 0 || blue < 0 || red > 255 || green > 255 || blue > 255 {
+            Err(IntoColorError::IntConversion)
+        } else {
+            Ok(Color {
+                red: red as u8,
+                green: green as u8,
+                blue: blue as u8,
+            })
+        }
     }
 }
 
-// Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        let red = slice[0];
+        let green = slice[1];
+        let blue = slice[2];
+        if red < 0 || green < 0 || blue < 0 || red > 255 || green > 255 || blue > 255 {
+            Err(IntoColorError::IntConversion)
+        } else {
+            Ok(Color {
+                red: red as u8,
+                green: green as u8,
+                blue: blue as u8,
+            })
+        }
     }
 }
-
 fn main() {
     // Use the `try_from` function
     let c1 = Color::try_from((183, 65, 14));
